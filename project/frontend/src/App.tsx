32be9f8e1f1df398df3df41aadeffc9c30ea8793
galdil <galdil@gmail.com>
2023-05-06 12:50:44 +0300

    front infra

---
+++ project/frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+import Inventory from '@pages/Inventory/Inventory';
+
+const App = (): JSX.Element => (
+  <div className="App">
+    <Inventory />
+    <br />
+  </div>
+);
+
+export default App;
---
+++ project/frontend/src/pages/Inventory/Inventory.tsx
@@ -0,0 +1,13 @@
+import Divider from '@mui/material/Divider';
+
+import './inventory.css';
+
+const Inventory = (): JSX.Element => (
+  <div className="sections-wrapper">
+    <div>hi</div>
+    <Divider orientation="vertical" sx={{ borderColor: 'white' }} />
+    <div>Gal</div>
+  </div>
+);
+
+export default Inventory;
